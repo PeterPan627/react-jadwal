8004ee5708e56ea8fc31e2cdf2bb8848be627ba2
LeiMing627 <jackperry933jp@gmail.com>
2018-08-23 11:24:25 +0700

    initiate jadwal component

---
+++ src/components/Jadwal.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Jadwal extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Jadwal</h1>
+      </div>
+    );
+  }
+}
+
+export default Jadwal;
